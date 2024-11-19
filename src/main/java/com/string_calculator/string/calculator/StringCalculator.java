f8be1b6b2bd276ac427ce2dc9ca6d048cc8820f6
Sohan <159467173+Sohanpatil07@users.noreply.github.com>
2024-11-19 14:19:27 +0530

    Handle empty string in add method

---
+++ src/main/java/com/string_calculator/string/calculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package com.string_calculator.string.calculator;
+
+public class StringCalculator {
+           public int add(String numbers) {
+            if (numbers.isEmpty()) {
+                return 0;
+            }
+            return 0;
+        }
+    }
+
+
--- src/test/java/com/string_calculator/StringCalculatorApplicationTests.java
+++ src/test/java/com/string_calculator/StringCalculatorApplicationTests.java
@@ -1,13 +1,18 @@
 package com.string_calculator;
 
+import com.string_calculator.string.calculator.StringCalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StringCalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testAddEmptyString() {
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(0, calculator.add(""));
 	}
 
 }
